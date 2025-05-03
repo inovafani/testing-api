bf9ee9834e4678446904c3314919100a01691024
Inov Afani <inoveka@gmail.com>
2025-05-03 23:52:57 +0700

    Updating the refresh page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $("#search-button").on("click", function () {
+  $("#movie-list").html("");
   $.ajax({
     url: "http://omdbapi.com",
     type: "get",
@@ -13,17 +14,19 @@ $("#search-button").on("click", function () {
         $.each(movies, function (i, data) {
           $("#movie-list").append(`
             <div class="col-md-4">
-                <div class="card">
+                <div class="card mb-3">
                  <img src="${data.Poster}" class="card-img-top" alt="...">
                  <div class="card-body">
-                 <h5 class="card-title">Card title</h5>
-                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <a href="#" class="btn btn-primary">Go somewhere</a>
+                 <h5 class="card-title">${data.Title}</h5>
+                 <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
+                 <a href="#" class="btn btn-primary">Details</a>
                  </div>
                 </div>
             </div>
             `);
         });
+
+        $("#search-input").val("");
       } else {
         $("#movie-list").html(`
             <div class="col">
