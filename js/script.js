8665181cc31db3338c76c23397b941c9774e4a6b
Inov Afani <inoveka@gmail.com>
2025-05-03 21:21:05 +0700

    update the posters feature

--- js/script.js
+++ js/script.js
@@ -8,7 +8,29 @@ $("#search-button").on("click", function () {
       s: $("#search-input").val(),
     },
     success: function (result) {
-      console.log(result);
+      if (result.Response == "True") {
+        let movies = result.Search;
+        $.each(movies, function (i, data) {
+          $("#movie-list").append(`
+            <div class="col-md-4">
+                <div class="card">
+                 <img src="${data.Poster}" class="card-img-top" alt="...">
+                 <div class="card-body">
+                 <h5 class="card-title">Card title</h5>
+                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                 <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+                </div>
+            </div>
+            `);
+        });
+      } else {
+        $("#movie-list").html(`
+            <div class="col">
+            <h1 class="text-center">${result.Error}</h1>
+            </div>
+            `);
+      }
     },
   });
 });
