7bdb978a41b725b3e2caf6d2fb91b9d9b433d12a
Inov Afani <inoveka@gmail.com>
2025-05-04 02:06:34 +0700

    Finishing the modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$("#search-button").on("click", function () {
+function searchMovie() {
   $("#movie-list").html("");
   $.ajax({
     url: "http://omdbapi.com",
@@ -19,7 +19,8 @@ $("#search-button").on("click", function () {
                  <div class="card-body">
                  <h5 class="card-title">${data.Title}</h5>
                  <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
-                 <a href="#" class="btn btn-primary">Details</a>
+                 <a href="#" class="btn btn-primary see-detail" data-toggle="modal"
+                 data-target="#exampleModal" data-id="${data.imdbID}">Details</a>
                  </div>
                 </div>
             </div>
@@ -33,6 +34,51 @@ $("#search-button").on("click", function () {
             <h1 class="text-center">${result.Error}</h1>
             </div>
             `);
+      }
+    },
+  });
+}
+
+$("#search-button").on("click", function () {
+  searchMovie();
+});
+
+$("#search-input").on("keyup", function (e) {
+  if (e.keyCode === 13) {
+    searchMovie();
+  }
+});
+
+$("#movie-list").on("click", ".see-detail", function () {
+  $.ajax({
+    url: "http://omdbapi.com",
+    type: "get",
+    dataType: "json",
+    data: {
+      apikey: "7e195fe4",
+      i: $(this).data("id"),
+    },
+    success: function (movie) {
+      if (movie.Response === "True") {
+        $(".modal-body").html(`
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="${movie.Poster}" class="img-fluid">
+                </div>
+ 
+                <div class="col-md-8">
+                <ul class="list-group">
+                 <li class="list-group-item"><h3>${movie.Title}</h3></li>
+                 <li class="list-group-item">Released : ${movie.Released}</li>
+                 <li class="list-group-item">Genre : ${movie.Genre}</li>
+                 <li class="list-group-item">Director : ${movie.Director}</li>
+                 <li class="list-group-item">Actors : ${movie.Actors}</li>
+                </ul>
+                </div>
+              </div>
+            </div>
+        `);
       }
     },
   });
